c4a903e0e0c2dd46d52a46ee27ef647aebfe3a1e
Roni1223 <113419582+Roni1223@users.noreply.github.com>
2022-09-15 08:19:06 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(input.acceleration(Dimension.X))
+    basic.showNumber(input.acceleration(Dimension.Y))
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
